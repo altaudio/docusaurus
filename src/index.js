be95079cc4e5181acf9c6a6a2abc021c4026bb71
Alex Smith <altaudiolabs@gmail.com>
2019-08-09 19:09:03 +0100

    Sets up webpack and react

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Renderer from './components/Renderer';
+
+const root = document.getElementById('root');
+render(<Renderer />, root);
